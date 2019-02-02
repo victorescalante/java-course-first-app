d82a931518c7f991457244b4cd6d9b799a0406fb
Victor Escalante <victorlt.xd3@gmail.com>
2019-02-02 13:59:23 -0600

    ejecicio con switch

--- src/cliente/Prueba.java
+++ src/cliente/Prueba.java
@@ -2,16 +2,36 @@ package cliente;
 
 public class Prueba {
     public static void main(String[] args){
-        Persona[] arrPersonas = new Persona[5];
-        System.out.println("arreglo: "+ arrPersonas[0]);
-        arrPersonas[0] = new Persona();
-        arrPersonas[1] = new Persona();
-        arrPersonas[2] = new Persona();
-        arrPersonas[3] = new Persona();
-        arrPersonas[4] = new Persona();
-        System.out.println("arreglo: "+ arrPersonas[0]);
-        System.out.println("arreglo: "+ arrPersonas[1]);
-        System.out.println("arreglo: "+ arrPersonas[2]);
-        System.out.println("arreglo: "+ arrPersonas[3]);
+        int opcion = 10;
+        switch (opcion){
+            case 10:
+                System.out.println("Cafe");
+                break;
+            case 20:
+                System.out.println("Otra");
+                break;
+            case 30:
+                System.out.println("Otra 1");
+                break;
+            default:
+                System.out.println("Ninguna");
+        }
+
     }
+
+
+
+    /*
+    Persona[] arrPersonas = new Persona[5];
+    System.out.println("arreglo: "+ arrPersonas[0]);
+    arrPersonas[0] = new Persona();
+    arrPersonas[1] = new Persona();
+    arrPersonas[2] = new Persona();
+    arrPersonas[3] = new Persona();
+    arrPersonas[4] = new Persona();
+    System.out.println("arreglo: "+ arrPersonas[0]);
+    System.out.println("arreglo: "+ arrPersonas[1]);
+    System.out.println("arreglo: "+ arrPersonas[2]);
+    System.out.println("arreglo: "+ arrPersonas[3]);
+    */
 }
